57dea5a250ae21c4531677522b6136d9ff83a661
Rajat Verma <rajat.verma@plootus.com>
2020-03-19 23:10:11 +0530

    Updates

---
+++ precache-manifest.178d9ff082e1b6ef2de5c85698867aa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3a6a87a93036bdca432c790b4c1d8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59eaaee4dbf657dea32e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "74b6c3146ae9b883bb97",
+    "url": "/static/js/2.9046b607.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.9046b607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59eaaee4dbf657dea32e",
+    "url": "/static/js/main.e11fcceb.chunk.js"
+  },
+  {
+    "revision": "08c18688b688174884eb",
+    "url": "/static/js/runtime-main.7d96d41e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
