ad00b3c1b4ce380fce67b2965ff6edff13ac5d09
Rajat Verma <rajat.verma@plootus.com>
2020-03-19 23:14:02 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Portfolio under development
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/rajat-verma/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Rajat Verma
         </a>
       </header>
     </div>
